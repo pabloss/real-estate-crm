06be4f8d92732a4ba9a6ac0710d0f5ab09674f29
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2026-06-13 13:03:29 +0200

    JWT

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from "axios";
+
+// --- Konfiguracja JWT Interceptora ---
+axios.interceptors.request.use((config) => {
+    const token = localStorage.getItem('jwt_token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+// -------------------------------------
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
